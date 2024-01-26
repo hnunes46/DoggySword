34e90cffd9e365b8728956361f3e151c82411396
Helder Nunes <helder.nunes@farfetch.com>
2024-01-26 13:52:43 +0000

    Initial Commit

---
+++ DoggySword/DoggySwordApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoggySwordApp.swift
+//  DoggySword
+//
+//  Created by Helder Nunes on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct DoggySwordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
